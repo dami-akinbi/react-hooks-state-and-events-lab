085a314ceb886db3b64bd29270a1a81c5dd71f95
damilolaakinbi <akinbidamilola074@gmail.com>
2023-02-15 23:08:43 +0100

    added some css styles, implemented add to cart feature

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [toggleCart, setToggleCart] = useState(false);
+
+  function handleItemClick() {
+    setToggleCart((toggleCart) => !toggleCart);
+    // console.log(event.target.parentNode);
+    // event.target.parentNode.classList.add("in-cart");
+  }
+
   return (
-    <li className="">
+    <li className={toggleCart ? `in-cart` : ``}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleItemClick}>
+        {toggleCart ? `Remove From Cart` : `Add to Cart`}
+      </button>
     </li>
   );
 }
